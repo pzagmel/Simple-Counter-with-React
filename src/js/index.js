ad0d4593d8ac693bdec15d7b8df34b5dc26c6b79
pzagmel <pedro.zagalm@gmail.com>
2022-11-14 15:58:28 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+const SecondsCounter = (props) => {
+	return (
+		<div className= "Contador">
+	
+		<div className="reloj"><i className="fa-solid fa-clock"></i></div>
+		<div className="seis">0</div>
+        <div className="cinco">0</div>
+        <div className="cuatro">0</div>
+		<div className="tres">0</div>
+		<div className="dos">0</div>
+		<div className="uno">0</div>
+		</div>
+	);
+};
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SecondsCounter />, document.querySelector("#app"));
