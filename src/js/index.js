7c842dd072a3c10472cd3116f431635fa365cc99
pzagmel <pedro.zagalm@gmail.com>
2022-11-17 17:27:17 +0000

    last commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 //import react into the bundle
-import propTypes from "prop-types";
 import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
@@ -12,7 +11,7 @@ const SecondsCounter = (props) => {
   return (
     <div className="Contador">
       <div className="reloj">
-        <i className="fa-solid fa-clock"> </i>{" "}
+        <i className="fas fa-clock"> </i>{" "}
       </div>{" "}
       <div className="seis"> {props.digitoSeis} </div>{" "}
       <div className="cinco"> {props.digitoCinco} </div>{" "}
@@ -23,14 +22,15 @@ const SecondsCounter = (props) => {
     </div>
   );
 };
-SecondsCounter.PropTypes = {
-  digitoSeis: PropTypes.number,
-  digitoCinco: PropTypes.number,
+SecondsCounter.propTypes = {
+  digitoSeis: PropTypes.string,
+  digitoCinco: PropTypes.string,
   digitoCuatro: PropTypes.number,
   digitoTres: PropTypes.number,
   digitoDos: PropTypes.number,
   digitoUno: PropTypes.number,
 };
+
 let counter = 0;
 const tiempo = {
   seg: 0,
@@ -41,39 +41,62 @@ const tiempo = {
   dhra: 0,
 };
 
+let hrs = 0;
 setInterval(function () {
   if (counter !== 0) {
-    tiempo.dhra = 0;
-    tiempo.hra = 0;
-    tiempo.dmin = 0;
+    if (counter % 3600===0) {
+      hrs += 1;
+      if (hrs == 24) hrs = 0;
 
+      let string = hrs.toString();
+      tiempo.hra = string[string.length - 1];
+      tiempo.dhra = string[string.length - 2];
+
+      if (! tiempo.dhra){
+       tiempo.dhra = 0;
+      }
+    }
+
+    if (counter % 600 === 0) {
+      tiempo.dmin += 1;
+      if (tiempo.dmin === 6) tiempo.dmin = 0;
+    }
     if (counter % 60 === 0 && counter != 0) {
       tiempo.min += 1;
       if (tiempo.min % 10 === 0) {
         tiempo.min = 0;
       }
     }
-   
-    
-    if (counter% 10 === 0) {
-		tiempo.dseg += 1;
-		if(tiempo.dseg===10) tiempo.dseg=0
 
+    if (counter % 10 === 0) {
+      tiempo.dseg += 1;
+      if (tiempo.dseg === 6) tiempo.dseg = 0;
     }
 
-      tiempo.seg += 1;
-    if(tiempo.seg===10) tiempo.seg=0
-}
-console.log(
-  tiempo.dhra,
-  tiempo.hra,
-  tiempo.dmin,
-  tiempo.min,
-  tiempo.dseg,
-  tiempo.seg
-);
-counter++;
-}, 1000);
+    tiempo.seg += 1;
+    if (tiempo.seg === 10) tiempo.seg = 0;
+  }
+  console.log(hrs)
+  console.log(
+    tiempo.dhra,
+    tiempo.hra,
+    tiempo.dmin,
+    tiempo.min,
+    tiempo.dseg,
+    tiempo.seg
+  );
+  counter++;
+  ReactDOM.render(
+    <SecondsCounter
+      digitoUno={tiempo.seg}
+      digitoDos={tiempo.dseg}
+      digitoTres={tiempo.min}
+      digitoCuatro={tiempo.dmin}
+      digitoCinco={tiempo.hra}
+      digitoSeis={tiempo.dhra}
+    />,
+    document.querySelector("#app")
+  );
+}, 5);
 
 //render your react application
-ReactDOM.render(<SecondsCounter />, document.querySelector("#app"));
